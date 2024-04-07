b29b963ca42c97c4d5e16405b9638c6747949632
Ben McAdams <mcadams.benj@gmail.com>
2024-04-06 17:02:47 -0700

    fix: format

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import {
-	Message,
-	// import as useAssistant:
-	experimental_useAssistant as useAssistant,
-} from "ai/react";
-import { Button } from "./ui/button";
-import { Input } from "./ui/input";
-import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
-} from "./ui/select";
-import { useEffect, useRef, useState } from "react";
+import { Message, experimental_useAssistant as useAssistant } from "ai/react";
+import { useRef, useEffect, useState } from "react";
 
 const roleToColorMap: Record<Message["role"], string> = {
 	system: "red",
@@ -22,7 +9,7 @@ const roleToColorMap: Record<Message["role"], string> = {
 	function: "blue",
 	assistant: "green",
 	data: "orange",
-	tool: "",
+	tool: "purple",
 };
 
 const THEMES = [
@@ -35,10 +22,16 @@ const THEMES = [
 ];
 
 export default function Chat() {
-	const { status, messages, input, submitMessage, handleInputChange } =
-		useAssistant({ api: "/api/assistant" });
-	const messagesContainerRef = useRef<HTMLDivElement>(null);
+	const {
+		status,
+		messages,
+		input,
+		submitMessage,
+		setInput,
+		handleInputChange,
+	} = useAssistant({ api: "/api/assistant" });
 	const [theme, setTheme] = useState(THEMES[0]);
+	const messagesContainerRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
 		if (messagesContainerRef.current) {
@@ -48,66 +41,72 @@ export default function Chat() {
 	}, [messages]);
 
 	return (
-		<div
-			className="flex flex-col w-full max-w-xl py-24 mx-auto stretch"
-			ref={messagesContainerRef}
-		>
-			{messages.map((m: Message) => (
-				<div
-					key={m.id}
-					className="whitespace-pre-wrap"
-					style={{ color: roleToColorMap[m.role] }}
-				>
-					<strong>{`${m.role}: `}</strong>
-					{m.role !== "data" && m.content}
-					{m.role === "data" && (
-						<>
-							{/* here you would provide a custom display for your app-specific data:*/}
-							{(m.data as any).description}
-							<br />
-							<pre className={"bg-gray-200"}>
-								{JSON.stringify(m.data, null, 2)}
-							</pre>
-						</>
-					)}
-					<br />
-					<br />
-				</div>
-			))}
-
-			{status === "in_progress" && (
-				<div className="h-8 w-full p-2 mb-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse" />
-			)}
+		<div className="stretch mx-auto flex h-screen w-full max-w-md flex-col pb-24 pt-8">
+			<div className="mb-24 w-full overflow-auto" ref={messagesContainerRef}>
+				{messages.map((m: Message) => (
+					<div
+						key={m.id}
+						className="whitespace-pre-wrap"
+						style={{ color: roleToColorMap[m.role] }}
+					>
+						<strong>{`${m.role}: `}</strong>
+						{m.role !== "data" && m.content}
+						{m.role === "data" && (
+							<>
+								{/* here you would provide a custom display for your app-specific data:*/}
+								{(m.data as any).description}
+								<br />
+								<pre className={"bg-gray-200"}>
+									{JSON.stringify(m.data, null, 2)}
+								</pre>
+							</>
+						)}
+						<br />
+						<br />
+					</div>
+				))}
+				{status === "in_progress" && (
+					<div className="flex justify-end pr-4">
+						<span className="animate-bounce">...</span>
+					</div>
+				)}
+			</div>
 
 			<form
 				onSubmit={submitMessage}
-				className="fixed bottom-0 max-w-xl w-full p-2 mb-8 space-y-4"
+				className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 shadow-xl bg-slate-200 rounded"
 			>
-				<Select defaultValue={theme}>
-					<SelectTrigger>
-						<SelectValue placeholder="Themes" />
-					</SelectTrigger>
-					<SelectContent>
-						{THEMES.map((opt, idx) => (
-							<SelectItem key={idx} value={opt}>
-								{opt}
-							</SelectItem>
-						))}
-					</SelectContent>
-				</Select>
-				<Input
+				<input
 					disabled={status !== "awaiting_message"}
+					className="w-full max-w-md p-2 mb-2 border bg-white border-gray-300 rounded shadow-xl"
 					value={input}
-					placeholder="Provide a short description of a painting"
+					placeholder="Describe a painting..."
 					onChange={handleInputChange}
 				/>
-				<Button
-					className="flex grow"
-					disabled={status !== "awaiting_message"}
-					type="submit"
+				<select
+					id="countries"
+					className="w-full max-w-md p-2 mb-2 border border-gray-300 rounded shadow-xl dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 bg-white"
+					value={theme}
+					onChange={(e) => setTheme(e.target.value)}
+					disabled={status === "in_progress"}
 				>
-					Send
-				</Button>
+					{THEMES.map((theme, idx) => (
+						<option key={idx} value={theme}>
+							{theme}
+						</option>
+					))}
+				</select>
+				<div className="flex flex-col">
+					<button
+						className="bg-blue-500 p-2 text-white rounded shadow-xl"
+						disabled={status === "in_progress"}
+						onClick={() =>
+							setInput(`Describe a a painting using the theme ${theme}`)
+						}
+					>
+						Describe painting
+					</button>
+				</div>
 			</form>
 		</div>
 	);
