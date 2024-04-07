0c5e4b3f6f8d9f990c072d78681bb0c8ec8f4e67
Ricy <ricyzhang951@gmail.com>
2024-04-07 10:47:57 -0700

    feat: add image data test part

--- app/api/img/route.ts
+++ app/api/img/route.ts
@@ -9,10 +9,10 @@ const openai = new OpenAI({
 export const runtime = 'edge';
 
 export async function POST(req: Request) {
-  // Parse the request body
   const input: {
     prompt: string;
   } = await req.json();
+  //TODO:Test
   const res = await openai.images.generate({
     model: 'dall-e-2',
     prompt: input.prompt,
@@ -21,4 +21,12 @@ export async function POST(req: Request) {
   });
 
   return NextResponse.json(res);
+
+  // return NextResponse.json({
+  //   data: [
+  //     {
+  //       url: 'https://img.freepik.com/free-psd/freshly-baked-pizza-with-cut-slice-isolated-transparent-background_191095-9041.jpg?w=996&t=st=1712510930~exp=1712511530~hmac=58ae217e0cc2583d8e95f19c387bfe20046460e5c2a67b7d7c16d43a713f1a59',
+  //     },
+  //   ],
+  // });
 }
